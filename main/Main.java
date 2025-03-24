e50400e7d710c4254131441bedcc7c9ac9f09dd5
ingxrd <ingridmunhoz86@gmail.com>
2025-03-24 09:13:09 -0300

    add outras infos

--- main/Main.java
+++ main/Main.java
@@ -1,3 +1,32 @@
+import java.util.ArrayList;
 public class Main {
+    public static void main(String[] args) {
+        InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
+
+        // 1o passo após chamar a interface é pedir as infos de valor do imovel, fazo de financiamento e taxa anual.
+
+        double valorImovel = interfaceUsuario.pedirValorImovel();
+        int prazoFinanciamento = interfaceUsuario.pedirPrazoFinanciamento(); 
+        double taxaJurosAnual = interfaceUsuario.pedirTaxaJurosAnual();
+
+        // 2o passo é instanciar objetos:
+
+        Financiamento meuFinanciamento = new Financiamento(valorImovel, prazoFinanciamento, taxaJurosAnual);
+        
+        // Arraylist para armazenar os financiamentos 
+        ArrayList<Integer> financiamentos = new ArrayList<Integer>();
+        financiamentos.add(meuFinanciamento);
+        for (int i : financiamentos){
+            System.out.println(i);
+        }
+        
+
+
+        // exibir total dos valores dos imoveis 
+
+        // exibir total dos valores dos financiamentos 
+
+    }
 
 }
+
