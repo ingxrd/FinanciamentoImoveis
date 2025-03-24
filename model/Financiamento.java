99a6714e43c738771817a859be95d99c32351181
cingrid <devingridmunhoz@gmail.com>
2025-03-24 00:56:53 -0300

    Criação de tres packages e as classes principais

---
+++ model/Financiamento.java
@@ -0,0 +1,61 @@
+public class Financiamento {
+    // Atributos privados
+    private double valorImovel;
+    private int prazoFinanciamento;
+    private double taxaJurosAnual;
+
+    //construtor
+    public Financiamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual){
+        this.valorImovel = valorImovel;
+        this.prazoFinanciamento = prazoFinanciamento;
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+
+    //Métodos getters e setter
+
+    //Método get e set = valorImovel 
+
+    public double getValorImovel(double valorImovel){
+        return valorImovel;
+    }
+
+    public void setValorImovel(double valorImovel){
+        this.valorImovel = valorImovel;
+    }
+
+    // Método get e set = prazoFinanciamento
+    public int getPrazoFinanciamento(int prazoFinanciamento){
+        return prazoFinanciamento;
+    }
+
+    public void setPrazoFinanciamento(int prazoFinanciamento){
+        this.prazoFinanciamento = prazoFinanciamento;
+    }
+
+    //Método get e set = taxaJurosAnual
+    public int getTaxaJurosAnual(int taxaJurosAnual){
+        return taxaJurosAnual;
+    }
+
+    public void setTaxaJurosAnual(int taxaJurosAnual){
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+
+
+    // MÉTODOS
+
+    // Método que calcula pagamento mensal do financiamento. 
+
+    public double calculoPagamentoMensal(){
+        double pagamentoMensal = (valorImovel / (prazoFinanciamento * 12)) * (1 + (taxaJurosAnual / 12));
+        return pagamentoMensal;
+    }
+
+    public double calculoPagamentoTotal(){
+        double pagamentoMensal = calculoPagamentoMensal();
+        double pagamentoTotal = pagamentoMensal * prazoFinanciamento * 12;
+        return pagamentoTotal;
+    }
+
+    
+}
---
+++ util/InterfaceUsuario.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+public class InterfaceUsuario {
+
+    // Melhor escolha: criar um construtor para inicializar o Scanner, evitando repeti-lo em cada um dos métodos de inserção de dados.
+    private Scanner scanner;
+
+    public InterfaceUsuario() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    // MÉTODOS 
+    // Método para pedir valor do imóvel. 
+    public double pedirValorImovel() {
+        double valorImovel;
+        while (true) {
+            System.out.print("Digite o valor do imóvel: ");
+            valorImovel = scanner.nextDouble();
+            if (valorImovel > 0) {
+                break;
+            } else {
+                System.out.println("O valor do imóvel deve ser positivo. ");
+            }
+        }
+        return valorImovel;
+    }
+
+    public int pedirPrazoFinanciamento() {
+        int prazoFinanciamento;
+        while (true) {
+            System.out.print("Insira o prazo de financiamento do imóvel em anos: ");
+            prazoFinanciamento = scanner.nextInt();
+            if (prazoFinanciamento > 0) {
+                break;
+            } else {
+                System.out.print("O prazo de financiamento deve ser um número positivo. ");
+            }
+        }
+        return prazoFinanciamento;
+    }
+
+    public double pedirTaxaJurosAnual() {
+        double taxaJurosAnual;
+        while (true) {
+            System.out.println("Insira a taxa de juros anual: ");
+            taxaJurosAnual = scanner.nextDouble();
+            if (taxaJurosAnual > 0) {
+                break;
+            } else {
+                System.out.print("A taxa de juros anual deve ser um número positivo. ");
+            }
+        }
+        return taxaJurosAnual;
+    }
+}
